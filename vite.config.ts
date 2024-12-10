385b1e4529462f38e33637bd0b0f74a12867d21b
Ola Skoczylas <ola.skoczylas@tain.com>
2024-12-10 12:29:38 +0000

    base

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/order_form/src/main.tsx', // Replace with your repository name
+  base: '/order_form/', // Replace with your repository name
 })
